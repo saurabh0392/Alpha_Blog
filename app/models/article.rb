d0a62e94fff8e0219758e3238af7bb9aea77d1f7
Saurabh Sharan <saurabhsharan03@gmail.com>
2017-01-02 13:37:40 +0000

    Adding user_id to articles and adding debug

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+    belongs_to :user
     validates :title, presence: true, length: {minimum: 3, maximum: 30}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
